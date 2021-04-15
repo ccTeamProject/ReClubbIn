62b7687c73c7cfe278a3b99a2d582840880e4da6
JohannaMathieu <johannamathieu@yahoo.de>
2021-04-15 20:41:05 +0200

    finally got some music from an API

--- src/components/CanvasContent.js
+++ src/components/CanvasContent.js
@@ -4,28 +4,31 @@ import axios from 'axios';
 
 function CanvasContent() {
 const audioSourceWeb = 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/viper.mp3';
-const apiRoot = 'http://ws.audioscrobbler.com/2.0/'
+//const audioSourceTest = 'https://www.last.fm/music/Amy+Winehouse';
 //const audioSourceLocal = 'src/music/testMusic.mp3';
-const apiKey = '9e28bb08c62299629031788c32bd7c10';
-const tagName = 'jazz';
+const apiRoot = 'https://freesound.org/apiv2/'
+const apiKey = 'P4oNXak4GuVnmaP4XIdFa7NYpDmNugG5LmC0zFPK';
+const genre = 'jazz';
 const limit ='5';
-const apiMethod = 'tag.gettoptracks'
+const soundID ='456797';
+//const apiMethod = 'tag.gettoptracks'
 
 const [getMusic, setGetMusic] = React.useState([]);
+let audioSource = getMusic;
 
 React.useEffect(()=>{
     axios
-        .get(`${apiRoot}?method=${apiMethod}&tag=${tagName}&limit=${limit}&api_key=${apiKey}&format=json`)
+        .get(`${apiRoot}sounds/${soundID}/?&format=json&fields=id,name,previews&token=${apiKey}`)
         .then((response)=>response.data)
-        .then((data) => setGetMusic(data.tracks.track));
-    
+        .then((data) => data.previews)
+        .then((previews) => setGetMusic(previews["preview-hq-mp3"]))
 
 }, []);
 
     return (
     <div className='content'>
         <GenreControl />
-        <audio controls autoPlay={true} loop src={audioSourceWeb} >
+        <audio controls autoPlay={true} loop src={audioSource} >
             <p>Your browser does not support the <code>audio</code> element.</p>            
         </audio>
         {console.log(getMusic)};
