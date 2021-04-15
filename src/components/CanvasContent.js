15ca3914e26435787ce93ffd045a565fed297f5c
JohannaMathieu <johannamathieu@yahoo.de>
2021-04-15 11:53:11 +0200

    add a audio element with some music playing

--- src/components/CanvasContent.js
+++ src/components/CanvasContent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import GenreControl from './GenreControl';
 
 function CanvasContent() {
+const audioSourceWeb = 'https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/viper.mp3';
+const audioSourceLocal = 'src/music/testMusic.mp3';
+
+
     return (
     <div className='content'>
         <GenreControl />
+        <audio controls autoPlay={true} preload="auto" loop src={audioSourceWeb} >
+            <p>Your browser does not support the <code>audio</code> element.</p>            
+        </audio>
     </div>
     );
 };
