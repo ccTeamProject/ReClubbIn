3f726252b4c06a4cc26b8ffe8154b0b68fff6de8
Zahra Said <zahra@zahrasaid.org>
2021-04-16 12:28:42 +0200

    added buttons

--- src/components/GenreButton.js
+++ src/components/GenreButton.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+let technoButton = "Techno";
+let classicButton = "Classic";
+let jazzButton = "Jazz";
 
-function GenreButton(){
-    return (
-        <button>Genre</button>
-    );
-};
+// create a variable with an array that has the genre
+
+function GenreButton() {
+  return <button className="genre-button">{technoButton}</button>;
+}
 
 export default GenreButton;
---
+++ src/components/testGenreButton.js
@@ -0,0 +1,33 @@
+import React from "react";
+// let technoButton = "Techno";
+// let classicButton = "Classic";
+// let jazzButton = "Jazz";
+
+// create a variable with an array that has the genre
+
+//function that is about the genre button, make a division with a props parameter
+
+function GenreButton(props) {
+  // let musicButtons = ["Techno", "Classic", "Jazz"];
+  return <button onClick={() => props.action()}>{props.musicButtons}</button>;
+
+  // function WelcomeMessage(props) {
+  //   return (
+  //   <div>
+  //   <h1>Hello {props.name}!</h1>
+  //   </div>
+  //   );
+  //   }
+
+  //   <div className="App">
+  // <WelcomeMessage name="John" />
+  // <WelcomeMessage name="Peter" />
+  // </div>
+
+  // <div>
+  //   <button className="genre-button">{technoButton}</button>
+  //   <button className="genre-button">{jazzButton}</button>
+  // </div>
+}
+
+export default GenreButton;
