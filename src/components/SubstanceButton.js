822f5025a58c1e9b0227f52b44c3dc348ec61fc9
Feras Abu rashid <ferasaburashid@gmail.com>
2021-04-21 09:39:27 +0200

    add beer button

--- src/components/SubstanceButton.js
+++ src/components/SubstanceButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-function SubstanceButton(props) {
+function SubstanceButton({substanceName, addDrinks}){
     return (
-        <button className='substance-button'>{props.substanceName}</button>
+        <button className='substance-button'  onClick={addDrinks}>
+           {substanceName==="Beer"? <img style={{width:"50px"}} src="https://www.flaticon.com/svg/vstatic/svg/925/925764.svg?token=exp=1618529145~hmac=2ee216a56e44e950d974539f59b213b7"/> : null}
+        </button>
     );
 };
 
