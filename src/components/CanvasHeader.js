a3963221a699635e1a03981bddacf71ec09b6fc5
JohannaMathieu <johannamathieu@yahoo.de>
2021-04-28 15:35:09 +0200

    added smiley effect, moved 'back'-button to canvas-header, gave canvas-header+canvas-footer height 100px, canvas content takes the rest of available space

--- src/components/CanvasHeader.js
+++ src/components/CanvasHeader.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import VisitorsCounter from './VisitorsCounter';
 import VolumeControl from './VolumeControl';
+import {NavLink} from 'react-router-dom';
 
 const CanvasHeader = () => {
     return (
         <div className='canvas-header'>
             <VolumeControl />
+            <button style={{height:"40px"}}> 
+                <NavLink to="/genres">back to genre-selection</NavLink>
+            </button>  
             <VisitorsCounter />
         </div>
     );
