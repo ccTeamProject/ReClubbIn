201597989309270853c61b938b9f4a9f94e95ae8
sofia <s@sofias.fritz.box>
2021-04-29 16:12:38 +0200

    guides with useTransition & useSpring

--- src/components/OnboardingGuidance.js
+++ src/components/OnboardingGuidance.js
@@ -1,16 +1,58 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom';
-import {useTransition, animated} from 'react-spring';
+import {useSpring, useTransition, animated, config} from 'react-spring';
 import "../OnBoardingGuidance.css"
 
-export const OnboardingGuidance = () => {
+export const OnboardingGuidance = (props) => {
+    
+    const [guides] = useState([
+        {id:1, text: 'Choose your Stage'},
+        {id:2, text: 'Play around'},
+        {id:3, text: 'Tap In'},
+    ])
 
-   /*  const link = `/genres/`; */
-   const [guide1, setGuide1] =useState();
+    const [index, setIndex] = useState(0);
 
-    return (
-        <div className="guide-1">
-           {/*  <NavLink to ={link}>Tap In</NavLink> */}
-        </div>
-    )
-}
+    const heightProps = useSpring({
+        config: config.slow,
+        from: { height: '0px' },
+        to: { height: '700px' },
+      });
+
+      const opacityProps = useSpring({
+        config: config.molasses,
+        delay: 400,
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      });
+
+    const fadingTextPropsTransition = useTransition(guides[index], (guide) => guide.id, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        config: { duration: 3000 },
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setIndex((state) => (state + 1) % guides.length);
+        }, 4000);
+    
+        return () => clearInterval(interval);
+      }, []);
+      
+      return (
+       
+          <animated.div style={opacityProps}>
+           {fadingTextPropsTransition.map(({ guide, props, key }) => (
+                      <animated.div 
+                      key={key} 
+                      style={{ ...props, position: 'absolute' }}>
+                        <p>
+                          {guide.text}
+                        </p>
+                        </animated.div>))}
+                        </animated.div>
+                     
+                    );
+                };
