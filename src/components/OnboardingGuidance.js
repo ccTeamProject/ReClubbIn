d8cfe7f110a2126866bb9c5f77549f1144f301f6
sofia <s@sofias.fritz.box>
2021-04-29 13:23:30 +0200

    install react-spring

--- src/components/OnboardingGuidance.js
+++ src/components/OnboardingGuidance.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
+import {useTransition, animated} from 'react-spring';
 import "../OnBoardingGuidance.css"
 
 export const OnboardingGuidance = () => {
 
-    const link = `/genres/`;
+   /*  const link = `/genres/`; */
+   const [guide1, setGuide1] =useState();
 
     return (
-        <div className="guide-button">
-            <NavLink to ={link}>Tap In</NavLink>
+        <div className="guide-1">
+           {/*  <NavLink to ={link}>Tap In</NavLink> */}
         </div>
     )
 }
