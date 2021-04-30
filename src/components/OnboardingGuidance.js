a455b874428f9641a1b1477deaa6f2a5a17b99d3
sofia <s@sofias.fritz.box>
2021-04-30 10:11:30 +0200

    creating useEffect & setInterval

--- src/components/OnboardingGuidance.js
+++ src/components/OnboardingGuidance.js
@@ -5,54 +5,33 @@ import "../OnBoardingGuidance.css"
 
 export const OnboardingGuidance = (props) => {
     
-    const [guides] = useState([
+    const guides = [
         {id:1, text: 'Choose your Stage'},
         {id:2, text: 'Play around'},
         {id:3, text: 'Tap In'},
-    ])
+    ];
 
     const [index, setIndex] = useState(0);
 
-    const heightProps = useSpring({
-        config: config.slow,
-        from: { height: '0px' },
-        to: { height: '700px' },
-      });
-
-      const opacityProps = useSpring({
-        config: config.molasses,
-        delay: 400,
-        from: { opacity: 0 },
-        to: { opacity: 1 },
-      });
-
-    const fadingTextPropsTransition = useTransition(guides[index], (guide) => guide.id, {
-        from: { opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
-        config: { duration: 3000 },
-    });
-
-    useEffect(() => {
+        useEffect(() => {
         const interval = setInterval(() => {
-          setIndex((state) => (state + 1) % guides.length);
-        }, 4000);
+          if(index>2){
+            console.log('dont do it')
+          }
+          else{
+            setIndex(index + 1);
+          }
+          }, 1000);
     
         return () => clearInterval(interval);
       }, []);
       
       return (
-       
-          <animated.div style={opacityProps}>
-           {fadingTextPropsTransition.map(({ guide, props, key }) => (
-                      <animated.div 
-                      key={key} 
-                      style={{ ...props, position: 'absolute' }}>
-                        <p>
-                          {guide.text}
-                        </p>
-                        </animated.div>))}
-                        </animated.div>
+          <p className="guide-button">
+            {guides[index].text}
+          </p>
+                       
+                       
                      
                     );
                 };
